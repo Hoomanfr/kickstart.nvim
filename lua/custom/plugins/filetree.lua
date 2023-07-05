06a17e6334f1d826f08e1df8dbffdec5942edfa9
Hooman Familrouhani <hooman.fr@gmail.com>
2023-07-04 17:57:43 -0700

    file tree

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,17 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require('neo-tree').setup {
+			buffers = {
+				follow_current_file = true,
+			},
+			vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>'),
+		}
+	end,
+}
