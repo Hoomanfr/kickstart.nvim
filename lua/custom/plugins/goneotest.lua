0b2e7953059ce7dbb3c16cb5671f9cd3adec50ce
Hooman Familrouhani <hooman.fr@gmail.com>
2023-12-07 01:15:55 -0800

    test

--- lua/custom/plugins/goneotest.lua
+++ lua/custom/plugins/goneotest.lua
@@ -17,24 +17,28 @@ return {
 			},
 		}, neotest_ns)
 		require("neotest").setup({
-			vim.keymap.set('n', '<leader>tf', ':lua require("neotest").run.run()<CR>', { noremap = true, silent = true }),
+			vim.keymap.set('n', '<leader>tf', ':lua require("neotest").run.run()<CR>',
+				{ noremap = true, silent = true, desc = "Test current Function" }),
 
-			vim.keymap.set('n', '<leader>ts', ':lua require("neotest").run.stop()<CR>', { noremap = true, silent = true }),
+			vim.keymap.set('n', '<leader>ts', ':lua require("neotest").run.stop()<CR>',
+				{ noremap = true, silent = true, desc = "Stop Test" }),
 
 
-			vim.keymap.set('n', '<leader>to', ':lua require("neotest").output.open()<CR>', { noremap = true, silent = true }),
+			vim.keymap.set('n', '<leader>to', ':lua require("neotest").output.open()<CR>',
+				{ noremap = true, silent = true, desc = "Test Output" }),
 			vim.keymap.set('n', '<leader>tO', ':lua require("neotest").output.open({enter=true})<CR>',
-				{ noremap = true, silent = true }),
+				{ noremap = true, silent = true, desc = "Test Output Enter" }),
 
-			vim.keymap.set('n', '<leader>ti', ':lua require("neotest").summary.toggle()<CR>', { noremap = true, silent = true }),
+			vim.keymap.set('n', '<leader>te', ':lua require("neotest").summary.toggle()<CR>',
+				{ noremap = true, silent = true, desc = "Test Explorer" }),
 			vim.keymap.set('n', '<leader>ta', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>',
-				{ noremap = true, silent = true }),
+				{ noremap = true, silent = true, desc = "Test All" }),
 
 			vim.keymap.set('n', '<leader>tp', "<cmd>lua require('neotest').jump.prev({status='failed'})<CR>",
-				{ noremap = true, silent = true }),
+				{ noremap = true, silent = true, desc = "Previouse failed Test" }),
 
 			vim.keymap.set('n', '<leader>tn', "<cmd>lua require('neotest').jump.next({status='failed'})<CR>",
-				{ noremap = true, silent = true }),
+				{ noremap = true, silent = true, desc = "Next failed Test" }),
 
 			adapters = {
 				require("neotest-go"),
