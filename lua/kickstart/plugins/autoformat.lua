f163db786725078fca4fe37c337fca4f7e1d98c1
Hooman Familrouhani <hooman.fr@gmail.com>
2023-12-06 19:12:12 -0800

    go auto imports

--- lua/kickstart/plugins/autoformat.lua
+++ lua/kickstart/plugins/autoformat.lua
@@ -2,6 +2,26 @@
 --
 -- Use your language server to automatically format your code on save.
 -- Adds additional commands as well to manage the behavior
+local golang_organize_imports = function(bufnr, isPreflight)
+  local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding(bufnr))
+  params.context = { only = { "source.organizeImports" } }
+
+  if isPreflight then
+    vim.lsp.buf_request(bufnr, "textDocument/codeAction", params, function() end)
+    return
+  end
+
+  local result = vim.lsp.buf_request_sync(bufnr, "textDocument/codeAction", params, 3000)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding(bufnr))
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
 
 return {
   'neovim/nvim-lspconfig',
@@ -49,7 +69,9 @@ return {
         if client.name == 'tsserver' then
           return
         end
-
+        if client.name == 'gopls' then
+          golang_organize_imports(bufnr, true)
+        end
         -- Create an autocmd that will run *before* we save the buffer.
         --  Run the formatting command for the LSP that has just attached.
         vim.api.nvim_create_autocmd('BufWritePre', {
@@ -58,6 +80,10 @@ return {
           callback = function()
             if not format_is_enabled then
               return
+            end
+
+            if client.name == 'gopls' then
+              golang_organize_imports(bufnr)
             end
 
             vim.lsp.buf.format {
